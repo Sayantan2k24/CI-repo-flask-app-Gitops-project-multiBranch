92b62cd06d910377fe0122aaef43d143505e569a
Sayantan <sayantansamanta12102001@gmail.com>
2025-07-19 23:37:57 +0530

    updated: content in / route

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "<b> Welcome to my project!! </b> <br/><br/> This is v2.0 release <br/>"
+    return "<b> Welcome to my project!! </b> <br/><br/> This update is for the build #2 of release v2.0"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
